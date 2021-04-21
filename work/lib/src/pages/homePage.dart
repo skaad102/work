2b47a1a8f8b39eb81cfbe7631f925c77ac62d614
John Doedaniel <skaad102@gmail.com>
2021-04-21 14:28:39 -0500

    bottom navigator

--- work/lib/src/pages/homePage.dart
+++ work/lib/src/pages/homePage.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:work/src/pages/favoritePage.dart';
+import 'package:work/src/pages/friendsPages.dart';
+import 'package:work/src/pages/messagePage.dart';
+import 'package:work/src/pages/profilePage.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
   static String nameRoute = 'home';
@@ -9,10 +14,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentPage = 3;
+  var pages = [FriendsPage(), MessagePage(), FavoritePage(), ProfilePage()];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(),
+    return SafeArea(
+      child: Container(
+        child: Scaffold(
+          body: Center(
+            child: pages.elementAt(currentPage),
+          ),
+          bottomNavigationBar: _customBNB(),
+        ),
+      ),
     );
+  }
+
+  Widget _customBNB() {
+    return BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.message_outlined), label: 'Mensajes'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border_outlined), label: 'Favoritos'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_outlined), label: 'Perfil'),
+        ],
+        currentIndex: currentPage,
+        fixedColor: Color.fromRGBO(254, 221, 124, 1),
+        onTap: (int inIndex) {
+          setState(() {
+            currentPage = inIndex;
+          });
+        });
   }
 }
