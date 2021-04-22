8be12a739580fa1ccbc3d55b5512cc28f303d389
John Doedaniel <skaad102@gmail.com>
2021-04-22 10:36:24 -0500

    pref

--- work/lib/main.dart
+++ work/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:work/block/providerBlock.dart';
+import 'package:work/models/prefUser.dart';
 import 'package:work/src/pages/homePage.dart';
 import 'package:work/src/pages/logPage.dart';
+import 'package:work/src/pages/regPage.dart';
 import 'package:work/src/pages/splashPage.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  final prefs = new PreferenciasUsuario();
+  WidgetsFlutterBinding.ensureInitialized();
+  await prefs.initPrefs();
+  await initHiveForFlutter();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -13,17 +22,19 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Material App',
         debugShowCheckedModeBanner: false,
-        initialRoute: SplashPage.nameRoute,
+        initialRoute: LogPage.nameRoute,
         routes: {
           HomePage.nameRoute: (_) => HomePage(),
           SplashPage.nameRoute: (_) => SplashPage(),
           LogPage.nameRoute: (_) => LogPage(),
+          RegPage.nameRoute: (_) => RegPage(),
         },
-        // theme: ThemeData(
-        //   textTheme: GoogleFonts.latoTextTheme(montserrat).copyWith(
-        //     bodyText2: GoogleFonts.oswald(textStyle: textTheme.body1),
-        //   ),
-        // ),
+        theme: ThemeData(
+            bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              // backgroundColor: Color.fromRGBO(254, 221, 124, 1),
+              unselectedItemColor: Colors.black,
+            ),
+            selectedRowColor: Color.fromRGBO(254, 221, 124, 1)),
       ),
     );
   }
